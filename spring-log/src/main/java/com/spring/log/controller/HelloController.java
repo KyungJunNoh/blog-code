946bd1d2951b34a0cdd4a2a6baf4b46f4446f42d
노경준 <developerjun0615@gmail.com>
2022-05-07 23:53:40 +0900

    logback spring boot

---
+++ spring-log/src/main/java/com/spring/log/controller/HelloController.java
@@ -0,0 +1,43 @@
+package com.spring.log.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    private Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @GetMapping("/hello")
+    public String hello(){
+        logger.info("hello!");
+        return "hello!";
+    }
+
+    @GetMapping("/errora")
+    public String error(){
+        logger.error("error!");
+        return "error!";
+    }
+
+    @GetMapping("/debug")
+    public String debug(){
+        logger.debug("debug!");
+        return "debug!";
+    }
+
+    @GetMapping("/warn")
+    public String warn(){
+        logger.warn("warn!");
+        return "debug!";
+    }
+
+    @GetMapping("/trace")
+    public String trace(){
+        logger.trace("trace!");
+        return "trace!";
+    }
+
+}
